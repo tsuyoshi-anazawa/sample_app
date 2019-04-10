9d724341953f0ff31cae945ef436a7ff7707f501
tsuyoshi-anazawa <anatsuyo0221@gmail.com>
2019-04-10 09:12:45 +0000

    Initial commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '63e4cd0191461e4d476e82c062c8e50690f6dd5ed0b3d0ee8b556b0438ea922ea18219b1516af96fbf8a39f2cfaed00d6b2e4e35b513d6c71d25d3e7ecd8f3a9'
